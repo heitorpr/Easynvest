27d31a8bbf91eacdd882a461a1ee73ac1a2d7255
Heitor Polizeli Rodrigues <heitorprodrigues@gmail.com>
2017-03-22 15:14:50 -0300

    Custom BottomNavigation with linear layout and buttons

--- EasyinvestTest/app/src/main/java/br/heitor/easyinvest/utils/HomeBottomNavListener.java
+++ EasyinvestTest/app/src/main/java/br/heitor/easyinvest/utils/HomeBottomNavListener.java
@@ -1,33 +1,56 @@
 package br.heitor.easyinvest.utils;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.graphics.drawable.TransitionDrawable;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import br.heitor.easyinvest.R;
 import br.heitor.easyinvest.views.widgets.FontTextView;
 
-public class HomeBottomNavListener {
-    public static BottomNavigationView.OnNavigationItemSelectedListener getListener(final AppCompatActivity activity, final FontTextView mTextMessage) {
-        return new BottomNavigationView.OnNavigationItemSelectedListener() {
+public class HomeBottomNavListener implements View.OnClickListener {
 
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_home:
-                        mTextMessage.setText(R.string.title_home);
-                        return true;
-                    case R.id.navigation_dashboard:
-                        mTextMessage.setText(R.string.title_dashboard);
-                        return true;
-                    case R.id.navigation_notifications:
-                        mTextMessage.setText(R.string.title_notifications);
-                        return true;
-                }
-                return false;
-            }
+    private FontTextView mTextMessage;
+    private int selected;
 
-        };
+    public HomeBottomNavListener(FontTextView mTextMessage, View selectedView) {
+        this.mTextMessage = mTextMessage;
+        this.selected = selectedView.getId();
+
+        selectView(selectedView, 0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (selected == v.getId()) return;
+        selected = v.getId();
+
+        if (v.getId() == R.id.btnNavInvestment) {
+            mTextMessage.setText(R.string.title_home);
+            selectView(v, 200);
+            return;
+        }
+
+        if (v.getId() != R.id.btnNavContact) return;
+
+        mTextMessage.setText(R.string.title_dashboard);
+        selectView(v, 200);
+    }
+
+    private void selectView(View v, int time) {
+        TransitionDrawable transition = (TransitionDrawable) v.getBackground();
+        transition.startTransition(time);
+
+        if (time == 0) return;
+        LinearLayout view = (LinearLayout) v.getParent();
+
+        if (v.getId() == R.id.btnNavInvestment) {
+            transition = (TransitionDrawable) view.findViewById(R.id.btnNavContact).getBackground();
+        }
+
+        if (v.getId() == R.id.btnNavContact) {
+            transition = (TransitionDrawable) view.findViewById(R.id.btnNavInvestment).getBackground();
+        }
+
+        transition.reverseTransition(0);
     }
 }
--- EasyinvestTest/app/src/main/java/br/heitor/easyinvest/views/activities/MainActivity.java
+++ EasyinvestTest/app/src/main/java/br/heitor/easyinvest/views/activities/MainActivity.java
@@ -1,12 +1,12 @@
 package br.heitor.easyinvest.views.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import br.heitor.easyinvest.R;
-import br.heitor.easyinvest.utils.FontManager;
 import br.heitor.easyinvest.utils.HomeBottomNavListener;
+import br.heitor.easyinvest.views.widgets.FontButtonView;
 import br.heitor.easyinvest.views.widgets.FontTextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,17 +14,25 @@ import butterknife.ButterKnife;
 public class MainActivity extends BaseAppCompatActivity {
     @BindView(R.id.message)
     FontTextView mTextMessage;
-    @BindView(R.id.navigation)
-    BottomNavigationView navigation;
     @BindView(R.id.content)
     FrameLayout content;
+    @BindView(R.id.btnTest)
+    FontButtonView btnTest;
+    @BindView(R.id.btnNavInvestment)
+    FontButtonView btnNavInvestment;
+    @BindView(R.id.btnNavContact)
+    FontButtonView btnNavContact;
+    @BindView(R.id.navigation)
+    LinearLayout navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
+        HomeBottomNavListener bottomNavListener = new HomeBottomNavListener(mTextMessage, btnNavInvestment);
 
-        navigation.setOnNavigationItemSelectedListener(HomeBottomNavListener.getListener(this, mTextMessage));
+        btnNavInvestment.setOnClickListener(bottomNavListener);
+        btnNavContact.setOnClickListener(bottomNavListener);
     }
 }
